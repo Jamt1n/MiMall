7535d43535c0ed3691b1d4cac33dd2ac46c38c83
lujt8687 <lujt8687@fxiaoke.com>
2021-05-26 18:29:52 +0800

    feat:接入三种mock方式

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,28 @@
 
 <script>
 export default {
-  name: 'App',
-  components: {
+  name: "App",
+  data() {
+    return {
+      res: {}
+    };
+  },
+  mounted() {
+    // 通过easy-mock平台实现
+    // this.axios.get('/user/login').then((res) => {
+    //   this.res = res;
+    // });
+    // 本地集成mockjs实现数据mock
 
+    this.axios.get("/user/login").then(res => {
+      this.res = res;
+    });
+    // 本地加载请求json文件的形式
+    // this.axios.get("/mock/user/login.json").then(res => {
+    //   this.res = res;
+    // });
   }
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,31 +1,36 @@
-import Vue from 'vue'
-import App from './App.vue'
-import axios from "axios"
-import VueAxios from "vue-axios"
-import router from "./router"
-import env from './env'
+import Vue from "vue";
+import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+import router from "./router";
+// import env from './env'
+
+const mock = true; // mock开关
+if (mock) {
+  require("./mock/api");
+}
 
 // 根据前端的跨域方式做调整  /a/b : /api/a/b => /a/b
 axios.defaults.baseURL = '/api'; //如果cros或者jsonp就要补全http://
 axios.defaults.timeout = 8000;
 // 根据环境变量获取不同的请求地址
-axios.defaults.baseURL = env.baseURL;
+// axios.defaults.baseURL = env.baseURL;
 // 接口错误拦截
-axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use(function(response) {
   let res = response.data;
   if (res.status == 0) {
     return res.data;
-  }else if (res.status == 10) {
-    window.location.href = '/#/login'
-  }else {
+  } else if (res.status == 10) {
+    window.location.href = "/#/login";
+  } else {
     alert(res.msg);
   }
 });
 
 Vue.use(VueAxios, axios);
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
