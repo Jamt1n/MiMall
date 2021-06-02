c36910be36d6500d816bbb50d8e6189476575b91
lujt8687 <lujt8687@fxiaoke.com>
2021-06-02 15:54:27 +0800

    feat:广告位实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -57,8 +57,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads-box"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/' + item.id" v-for="item in adsList" :key="item.id">
+          <img :src="item.img" alt="">
+        </a>
+      </div>
+      <div class="banner">
+        <a :href="'/#/product/30'">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
 
@@ -75,6 +83,24 @@ export default {
   components: { ServiceBar, Swiper, SwiperSlide },
   data() {
     return {
+      adsList: [
+        {
+          id: 33,
+          img: '/imgs/ads/ads-1.png'
+        },
+        {
+          id: 48,
+          img: '/imgs/ads/ads-2.jpg'
+        },
+        {
+          id: 45,
+          img: '/imgs/ads/ads-3.png'
+        },
+        {
+          id: 47,
+          img: '/imgs/ads/ads-4.jpg'
+        }
+      ],
       menuList: [
         [
           {
@@ -231,6 +257,18 @@ export default {
         height: 100%;
       }
     }
+  }
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a {
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner {
+    margin-bottom: 50px;
   }
 }
 </style>
