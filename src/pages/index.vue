edad098cbc4a73c3fd9ae0eb9b1c381a5fd5b8c5
lujt8687 <lujt8687@fxiaoke.com>
2021-06-02 15:41:56 +0800

    feat:轮播菜单实现

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,16 @@
           <ul class="menu-wrap">
             <li class="menu-item">
               <a href="javascript:;">手机 电话卡</a>
-              <div class="children"></div>
+              <div class="children">
+                <ul v-for="(item, index) in menuList" :key="index">
+                  <li v-for="sub in item" :key="sub.id">
+                    <a :href="sub ? '/#/product/'+ sub.id:''">
+                      <img :src="sub?sub.img : '/imgs/item-box-1.png'" alt="">
+                      {{sub ? sub.name : '小米9'}}
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </li>
             <li class="menu-item">
               <a href="javascript:;">电视 盒子</a>
@@ -66,6 +75,35 @@ export default {
   components: { ServiceBar, Swiper, SwiperSlide },
   data() {
     return {
+      menuList: [
+        [
+          {
+            id:30,
+            img:'/imgs/item-box-1.png',
+            name:'小米CC9'
+          },
+          {
+            id:31,
+            img:'/imgs/item-box-2.png',
+            name:'小米8青春版'
+          },
+          {
+            id:32,
+            img:'/imgs/item-box-3.jpg',
+            name:'Redmi K20 Pro'
+          },
+          {
+            id:33,
+            img:'/imgs/item-box-4.jpg',
+            name:'移动4G专区'
+          }
+        ],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0],
+        [0,0,0,0]
+      ],
       swiperOption: {
         autoplay: true,
         loop: true,
@@ -117,13 +155,77 @@ export default {
 .index {
   .swiper-box {
     .nav-menu {
+      box-sizing: border-box;
       position: absolute;
       width: 264px;
       height: 451px;
       z-index: 9;
+      padding: 26px 0;
+      background-color: #55585A7A;
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          a {
+            display: block;
+            position: relative;
+            font-size: 16px;
+            color: #ffffff;
+            padding-left: 30px;
+            &::after {
+              position: absolute;
+              right: 30px;
+              top: 17.5px;
+              content: " ";
+              @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
+            }
+          }
+          &:hover {
+            background-color: $colorA;
+            .children {
+              display: block;
+            }
+          }
+          .children {
+            display: none;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid $colorH;
+            ul {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              height: 75px;
+              li {
+                height: 75px;
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
+              }
+              a {
+                color: $colorB;
+                font-size: 14px;
+              }
+              img {
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 15px;
+              }
+            }
+          }
+        }
+      }
     }
     .swiper-container {
       height: 451px;
+      .swiper-button-prev {
+        left: 274px;
+      }
       img {
         width: 100%;
         height: 100%;
