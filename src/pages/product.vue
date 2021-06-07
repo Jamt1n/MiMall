0cc395f4c4a8f2ca630061c143aafb925cd94241
lujt8687 <lujt8687@fxiaoke.com>
2021-06-07 19:35:39 +0800

    feat:产品站参数组件实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>product</div>
+  <div class="product">
+    <product-param>
+      <template v-slot:buy>
+        <button class="btn" @click="buy">立即购买</button>
+      </template>
+    </product-param>
+  </div>
 </template>
 
 <script>
+import ProductParam from "../components/ProductParam";
 export default {
-  name: "product"
+  name: "product",
+  components: {
+    ProductParam
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+.product {
+  button {
+    margin-left: 10px;
+  }
+}
 </style>
